2624f875d895029ebbf90e3fb71f9ecf294651f7
Neil Mispelaar <neil.mispelaar@outlook.com>
2024-03-18 01:52:40 -0400

    Add image box

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,21 @@
     title="Turn Your Vision into Reality"
     description="Watch your dreams materialize before your eyes with us."
   />
-  <p class="text-red-500">
-    Hello world
-  </p>
+  <u-landing-section
+    title="The freedom to build anything"
+    description="Nuxt UI Pro ships with an extensive set of advanced components that cover a wide range of use-cases."
+    :links="[{
+      label: 'Explore components',
+      color: 'gray',
+      size: 'lg',
+    }]"
+    align="left"
+  >
+    <img
+      src="https://picsum.photos/360/640"
+      class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
+    >
+  </u-landing-section>
 </template>
 
 <style scoped>
