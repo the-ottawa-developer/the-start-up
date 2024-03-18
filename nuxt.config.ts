4e7b0aaf77eceae4ebc7f8017f53cbf6eb2c474a
Neil Mispelaar <neil.mispelaar@outlook.com>
2024-03-18 13:21:09 -0400

    Add another section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@nuxt/ui-pro'],
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', "@nuxt/image"],
   devtools: { enabled: true },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,21 @@
       class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
     >
   </u-landing-section>
+
+  <u-blog-list orientation="horizontal">
+    <u-blog-post
+      title="Nuxt 3.9"
+      description="Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5, interactive..."
+    />
+    <u-blog-post
+      title="Nuxt DevTools 1.0"
+      description="Nuxt DevTools v1.0 is out, generally available to all Nuxt projects!"
+    />
+    <u-blog-post
+      title="Nuxt 3.8"
+      description="Nuxt 3.8 is out, bringing built-in DevTools, automatic Nuxt Image install, a new app..."
+    />
+  </u-blog-list>
 </template>
 
 <style scoped>
